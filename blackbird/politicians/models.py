e8290619c9fe779a57546e4b203ce2315fbd2761
Deron <deronmcguire23@yahoo.com>
2023-02-19 15:59:36 -0600

    Added politician model and changed its name

--- blackbird/politicians/models.py
+++ blackbird/politicians/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+
+class Politician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField
+    party = models.CharField(max_length=50)
+    state = models.CharField(max_length=100)
+
+    @property
+    def age(self):
+        return datetime.date.today() - self.date_of_birth
