9b19f22b724254f1b3bef1d94e754823812ae4db
Deron <deronmcguire23@yahoo.com>
2023-02-27 20:10:45 -0600

    Registered new models in admin.py

--- blackbird/politicians/admin.py
+++ blackbird/politicians/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Politician
+from .models import Senator, Representative, President
 # Register your models here.
-
-admin.site.register(Politician)
+MODELS = [Senator, Representative, President]
+admin.site.register(MODELS)
