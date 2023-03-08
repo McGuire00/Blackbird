c5fbd1893dbda6eeb6c909650ce229efe34544a0
Deron <deronmcguire23@yahoo.com>
2023-03-07 22:33:44 -0600

    Added white space and changed rep field to sponsor

--- blackbird/bills/models.py
+++ blackbird/bills/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from ..politicians.models import Representative
 
+
 class Bill(models.Model):
     name = models.CharField(max_length=100)
-    representative = models.OneToOneField(
+    sponsor = models.OneToOneField(
         Representative,
         on_delete=models.CASCADE,
         primary_key=True,
