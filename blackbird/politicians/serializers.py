3ab0f252e1407367faa2932decae69075be1a6ea
Deron <deronmcguire23@yahoo.com>
2023-03-13 23:23:28 -0500

    Added Representative and President serializers

--- blackbird/politicians/serializers.py
+++ blackbird/politicians/serializers.py
@@ -6,3 +6,15 @@ class SenatorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Senator
         fields = ("__all__")
+
+
+class RepresentativeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Representative
+        fields = ("__all__")
+
+
+class PresidentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = President
+        fields = ("__all__")
