b2db7cc616e99073260f0d0f951edd7d33aef3af
Deron <deronmcguire23@yahoo.com>
2023-03-14 22:03:27 -0500

    Removed redundant parenthesis

--- blackbird/politicians/serializers.py
+++ blackbird/politicians/serializers.py
@@ -5,16 +5,16 @@ from .models import Senator, Representative, President
 class SenatorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Senator
-        fields = ("__all__")
+        fields = "__all__"
 
 
 class RepresentativeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Representative
-        fields = ("__all__")
+        fields = "__all__"
 
 
 class PresidentSerializer(serializers.ModelSerializer):
     class Meta:
         model = President
-        fields = ("__all__")
+        fields = "__all__"
