4c4c0e52dca5d77c42206214011a1d34a75c30c6
Deron <deronmcguire23@yahoo.com>
2023-03-17 11:31:52 -0500

    Added bills app to root url path

--- blackbird/blackbird/urls.py
+++ blackbird/blackbird/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("politicians.api.urls", namespace="politicians")),
+    path('api/', include("bills.api.urls", namespace="bills")),
 ]
