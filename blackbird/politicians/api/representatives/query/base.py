156bb698e7b2645c5d5ab30d82da53972c37ae37
Deron <deronmcguire23@yahoo.com>
2023-03-16 19:25:42 -0500

    added viewset skeleton with serializer for Representative model

---
+++ blackbird/politicians/api/representatives/query/base.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets
+from politicians.models import Representative
+from rest_framework import serializers
+from rest_framework.response import Response
+
+
+class RepresentativeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Representative
+        fields = "__all__"
+
+
+class RepresentativeViewSet(viewsets.ModelViewSet):
+    queryset = Representative.objects.all()
+    serializer_class = RepresentativeSerializer
+
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
