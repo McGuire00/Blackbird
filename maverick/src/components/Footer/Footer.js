00b08e0172bb1d9629cd65b8adb8bb3d1df25da5
Deron <deronmcguire23@yahoo.com>
2023-08-14 12:38:06 -0500

    Created default footer with styling

--- maverick/src/components/Footer/Footer.js
+++ maverick/src/components/Footer/Footer.js
@@ -1,9 +1,49 @@
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      Footer
+      <div className="footer-container">
+        <div className="footer-content">
+          <div className="footer-left">
+            <h3>About Us</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+              felis ut felis accumsan fringilla vitae at elit.
+            </p>
+          </div>
+          <div className="footer-middle">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-right">
+            <h3>Contact Us</h3>
+            <p>
+              Address: 1234 Main Street, City, Country
+              <br />
+              Email: contact@example.com
+              <br />
+              Phone: +1 (123) 456-7890
+            </p>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; 2023 Your Website Name. All rights reserved.</p>
+        </div>
+      </div>
     </div>
   );
 }
