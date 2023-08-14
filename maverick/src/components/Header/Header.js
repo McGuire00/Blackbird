20147fb64065ac3ec2da7a5256ff2607799a3ff4
Deron <deronmcguire23@yahoo.com>
2023-08-14 12:41:25 -0500

    Added simple header with styling

--- maverick/src/components/Header/Header.js
+++ maverick/src/components/Header/Header.js
@@ -3,7 +3,15 @@ import './Header.css';
 function Header() {
   return (
     <div className="header">
-      Header
+      <h1 className="header-title">Politician Dictionary</h1>
+      <nav className="header-nav">
+        <ul className="header-nav-list">
+          <li className="header-nav-item">Politicians</li>
+          <li className="header-nav-item">Congress</li>
+          <li className="header-nav-item">About</li>
+          <li className="header-nav-item">Contact</li>
+        </ul>
+      </nav>
     </div>
   );
 }
