06012f6149f7a5349164428fbf7ddc8abb15654e
Deron <deronmcguire23@yahoo.com>
2023-08-14 19:05:33 -0500

    Created a Component to handle all of the ImageCards being created

---
+++ maverick/src/components/ImageCardGroup/ImageCardGroup.js
@@ -0,0 +1,22 @@
+import "./ImageCardGroup.css"
+
+
+import ImageCard from "../ImageCard/ImageCard";
+function ImageCardGroup({cabinetMembers}) {
+  return (
+    <div className="image-card-container">
+      {cabinetMembers.cabinet.map((member, index) => {
+        const image = require(`../../assets/${member[index].image}`);
+        console.log(image)
+        return ( <ImageCard
+            key={index}
+            imageUrl={image}
+            name={member[index].name}
+            position={member[index].position}
+        />)
+      })}
+    </div>
+  );
+}
+
+export default ImageCardGroup;
