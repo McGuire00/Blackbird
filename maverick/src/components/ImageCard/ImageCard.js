7b1243bff83e94df44f1944530930729d7731952
Deron <deronmcguire23@yahoo.com>
2023-08-14 19:04:51 -0500

    Created ImageCard component with styling to create images for each cabinet memeber

---
+++ maverick/src/components/ImageCard/ImageCard.js
@@ -0,0 +1,13 @@
+import "./ImageCard.css"
+
+function ImageCard({imageUrl, name, position}) {
+  return (
+    <div className="image-card">
+      <img src={imageUrl} alt={name}></img>
+      <h2>{name}</h2>
+      <p>{position}</p>
+    </div>
+  );
+}
+
+export default ImageCard;
