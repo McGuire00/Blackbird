b4688063088cc56c0e6e9a38d0cd0f277d278078
Deron <deronmcguire23@yahoo.com>
2023-08-17 18:05:26 -0500

    Hooked up newly created PolitianList and CongressSideBar component to main Congress component passing in dummy data for testing

--- maverick/src/components/Congress/Congress.js
+++ maverick/src/components/Congress/Congress.js
@@ -1,8 +1,93 @@
-import './Congress.css';
+import { useState } from "react";
+
+import "./Congress.css";
+
+import CongressSideBar from "../CongressSideBar/CongressSideBar";
+import PoliticianList from "../PoliticianList/PoliticianList";
+// import senators from "../../util/118th_congress_senators.json";
+// import representatives from "../../util/118th_congress_reps.json";
 function Congress() {
+  // TODO MAKE THIS FUNCTIONAL
+
+  const people = {
+    members: [
+      {
+        position: "Senator",
+        name: "Tammy Baldwin",
+        state: "Wisconsin",
+        party: "Democratic",
+        image: "Tammy_Baldwin.jpg",
+      },
+      {
+        position: "Senator",
+        name: "John Barrasso",
+        state: "Wyoming",
+        party: "Republican",
+        image: "John_Barrasso.jpg",
+      },
+      {
+        position: "Senator",
+        name: "Michael F. Bennet",
+        state: "Colorado",
+        party: "Democratic",
+        image: "Michael F._Bennet.jpg",
+      },
+      {
+        position: "Senator",
+        name: "Marsha Blackburn",
+        state: "Tennessee",
+        party: "Republican",
+        image: "Marsha_Blackburn.jpg",
+      },
+      {
+        position: "Senator",
+        name: "Richard Blumenthal",
+        state: "Connecticut",
+        party: "Democratic",
+        image: "Richard_Blumenthal.jpg",
+      },
+      {
+        position: "Senator",
+        name: "Tammy Baldwin",
+        state: "Wisconsin",
+        party: "Democratic",
+        image: "Tammy_Baldwin.jpg",
+      },
+      {
+        position: "Senator",
+        name: "John Barrasso",
+        state: "Wyoming",
+        party: "Republican",
+        image: "John_Barrasso.jpg",
+      },
+      {
+        position: "Senator",
+        name: "Michael F. Bennet",
+        state: "Colorado",
+        party: "Democratic",
+        image: "Michael F._Bennet.jpg",
+      },
+      {
+        position: "Senator",
+        name: "Marsha Blackburn",
+        state: "Tennessee",
+        party: "Republican",
+        image: "Marsha_Blackburn.jpg",
+      },
+      {
+        position: "Senator",
+        name: "Richard Blumenthal",
+        state: "Connecticut",
+        party: "Democratic",
+        image: "Richard_Blumenthal.jpg",
+      },
+    ],
+  };
+
   return (
     <div className="congress">
-      Hello Congress
+      <CongressSideBar />
+      <PoliticianList people={people} />
     </div>
   );
 }
