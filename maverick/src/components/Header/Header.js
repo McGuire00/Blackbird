baedb59e936fd8959aa9a2ce1875b7d614a0ae08
Deron <deronmcguire23@yahoo.com>
2023-08-16 20:11:51 -0500

    Made header title a link to home and added styling to fix weird issue of links showing as clicked

--- maverick/src/components/Header/Header.js
+++ maverick/src/components/Header/Header.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className="header">
-      <h1 className="header-title">Politician Dictionary</h1>
+      <h1 className="header-title">
+        <Link className="nav-link" to="/">Politician Dictionary</Link>
+      </h1>
       <nav className="header-nav">
         <ul className="header-nav-list">
           <li className="header-nav-item">Politicians</li>
           <li className="header-nav-item">
-            <Link to="/congress">Congress</Link>
+            <Link className="nav-link" to="/congress">Congress</Link>
           </li>
           <li className="header-nav-item">About</li>
           <li className="header-nav-item">Contact</li>
