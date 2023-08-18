6e9a8f20d5f8bb20344856402529e9c248e2e3ba
Deron <deronmcguire23@yahoo.com>
2023-08-18 17:09:46 -0500

    Added labels for politician details

--- maverick/src/components/Politician/Politician.js
+++ maverick/src/components/Politician/Politician.js
@@ -5,9 +5,9 @@ function Politician({ position, name, state, party, district, imageUrl }) {
     <div className="politician-card">
       <img className="politician-image" src={imageUrl} alt={name}></img>
       <h4>{name}</h4>
-      <p>{position}</p>
-      <p>{state}</p>
-      <p>{party}</p>
+      <p>Position: {position}</p>
+      <p>State: {state}</p>
+      <p>Party: {party}</p>
       {/*<p>{district}</p>*/}
     </div>
   );
