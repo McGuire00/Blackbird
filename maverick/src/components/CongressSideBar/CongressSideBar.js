8fcec843545fc7fdfb8a19d224a1c5421af8063e
Deron <deronmcguire23@yahoo.com>
2023-08-21 13:42:04 -0500

    Changed CongressSideBar component to be in charge of creating Accordion components

--- maverick/src/components/CongressSideBar/CongressSideBar.js
+++ maverick/src/components/CongressSideBar/CongressSideBar.js
@@ -1,19 +1,88 @@
 import "./CongressSideBar.css";
 
+import Accordion from "../Accordion/Accordion";
+import { useState } from "react";
 function CongressSideBar() {
+  const filterData = [
+    {
+      title: "Congress",
+      options: ["118th Congress", "117th Congress", "116th Congress"],
+    },
+    {
+      title: "Chamber",
+      options: ["House", "Senate"],
+    },
+    {
+      title: "Party",
+      options: ["House", "Senate"],
+    },
+    {
+      title: "Members by US State or Territory",
+      options: [
+        "Alabama",
+        "Alaska",
+        "American Samoa",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "District of Columbia",
+        "Florida",
+        "Georgia",
+        "Guam",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Northern Mariana islands",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Puerto Rico",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Vermont",
+        "Virgin Islands",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming",
+      ],
+    },
+  ];
   return (
     <div className="sidebar">
-      <h2>Filter Members</h2>
-      <label htmlFor="party-filter">Party:</label>
-      {/*<select*/}
-      {/*  id="party-filter"*/}
-      {/*  value={filterParty}*/}
-      {/*  onChange={handleFilterChange}*/}
-      {/*>*/}
-      <option value="All">All</option>
-      <option value="Democratic">Democratic</option>
-      <option value="Republican">Republican</option>
-      {/*</select>*/}
+      {filterData.map((filter, index) => {
+        return <Accordion filters={filter} />;
+      })}
     </div>
   );
 }
