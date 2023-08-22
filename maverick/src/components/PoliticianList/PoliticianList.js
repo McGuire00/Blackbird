2eec85983923bf8351e3b82201f6be17e7984442
Deron <deronmcguire23@yahoo.com>
2023-08-22 13:27:13 -0500

    Component now takes in parent props, added filtering function, and handle cases where there or no matches

--- maverick/src/components/PoliticianList/PoliticianList.js
+++ maverick/src/components/PoliticianList/PoliticianList.js
@@ -1,10 +1,39 @@
 import "./PoliticianList.css";
 
 import Politician from "../Politician/Politician";
-function PoliticianList({ people }) {
+function PoliticianList({ people, filterOptions }) {
+  const filteredMembers = filterMembers(people.members, filterOptions);
+  // Function to filter members based on filterOptions
+  function filterMembers(members, filterOptions) {
+    if (filterOptions.Party && filterOptions.Party.length > 0) {
+      return members.filter((member) =>
+        filterOptions.Party.includes(member.party),
+      );
+    } else if (filterOptions.State && filterOptions.State.length > 0) {
+      return members.filter((member) =>
+        filterOptions.State.includes(member.state),
+      );
+    } else if (filterOptions.Chamber && filterOptions.Chamber.length > 0) {
+      return members.filter((member) =>
+        filterOptions.Chamber.includes(member.chamber),
+      );
+    }
+
+    // If no filters are selected, return all members
+    return members;
+  }
+
+  if (filteredMembers.length === 0) {
+    return (
+      <div className="no-results">
+        No results found for the selected filters.
+      </div>
+    );
+  }
+
   return (
     <div className="member-list-container">
-      {people.members.map((member, index) => {
+      {filteredMembers.map((member, index) => {
         const image = require(`../../assets/congress_sessions/118th_congress/senators/${member.image}`);
         return (
           <div key={index} className="member-item">
