5f4b92de7540f3cd91ff13b74ba0c52ba5ee6dad
Deron <deronmcguire23@yahoo.com>
2023-08-22 13:24:22 -0500

    Changed filter options to be a checkbox and wrote function to handle filter change

--- maverick/src/components/Accordion/Accordion.js
+++ maverick/src/components/Accordion/Accordion.js
@@ -1,20 +1,42 @@
 import { useState } from "react";
 import "./Accordion.css";
 
-function Accordion({ filters }) {
+function Accordion({ filters, setFilterOptions }) {
   const [isVisible, setIsVisible] = useState(false);
+  const [selectedFilters, setSelectedFilters] = useState([]);
   function renderAccordion() {
     if (filters.options) {
       return filters.options.map((option, index) => {
         return (
           <div className="list-item" key={index}>
-            <p>{option}</p>
+            <label key={index}>
+              <input
+                type="checkbox"
+                value={option}
+                checked={selectedFilters.includes(option)}
+                onChange={(e) => handleFilterChange(e, option)}
+              />
+              {option}
+            </label>
           </div>
         );
       });
     }
   }
+  const handleFilterChange = (e, option) => {
+    const selectedOption = option;
+    const updatedFilters = e.target.checked
+      ? [...selectedFilters, selectedOption]
+      : selectedFilters.filter((filter) => filter !== selectedOption);
 
+    setSelectedFilters(updatedFilters);
+
+    // Update filter options in the parent component using the updatedFilters value
+    setFilterOptions((prevFilterOptions) => ({
+      ...prevFilterOptions,
+      [filters.title]: updatedFilters,
+    }));
+  };
   return (
     <div className="accordion-container">
       <div className="accordion">
@@ -22,7 +44,7 @@ function Accordion({ filters }) {
           className="accordion-title"
           onClick={() => setIsVisible(!isVisible)}
         >
-          {filters.title}
+          {filters.label}
         </div>
       </div>
       <div className={`content ${isVisible ? "show-content" : ""}`}>
