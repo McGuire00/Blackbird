2dcd6d9e34a63c194519f8bf4b7671756ccac71b
Deron <deronmcguire23@yahoo.com>
2023-08-28 19:02:14 -0500

    Consolidated first and last name field into name field, commented out date of birth, and added image field

--- blackbird/politicians/models.py
+++ blackbird/politicians/models.py
@@ -3,11 +3,11 @@ import datetime
 
 
 class Politician(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    date_of_birth = models.DateField(max_length=8, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    # date_of_birth = models.DateField(max_length=8, null=True, blank=True)
     party = models.CharField(max_length=50)
     state = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='politician_images/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
