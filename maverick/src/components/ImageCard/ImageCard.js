9730f4a201ad0c919ff1c7651c197f8a1bd928f7
Deron <deronmcguire23@yahoo.com>
2023-09-09 23:31:37 -0500

    Undo test variable

--- maverick/src/components/ImageCard/ImageCard.js
+++ maverick/src/components/ImageCard/ImageCard.js
@@ -3,7 +3,6 @@ import "./ImageCard.css";
 function ImageCard({ imageUrl, name, position }) {
   return (
     <div className="image-card">
-      {test}
       <img src={imageUrl} alt={name}></img>
       <h2>{name}</h2>
       <p>{position}</p>
