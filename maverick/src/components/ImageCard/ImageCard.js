01dfd99b3498422352bbf7b74c47a9b212aa8985
Deron <deronmcguire23@yahoo.com>
2023-09-09 23:31:08 -0500

    test push

--- maverick/src/components/ImageCard/ImageCard.js
+++ maverick/src/components/ImageCard/ImageCard.js
@@ -1,8 +1,9 @@
-import "./ImageCard.css"
+import "./ImageCard.css";
 
-function ImageCard({imageUrl, name, position}) {
+function ImageCard({ imageUrl, name, position }) {
   return (
     <div className="image-card">
+      {test}
       <img src={imageUrl} alt={name}></img>
       <h2>{name}</h2>
       <p>{position}</p>
